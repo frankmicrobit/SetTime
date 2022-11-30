1142f8fcd861ab31189b7cf33f55a865974ca588
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2022-11-30 12:36:44 +0100

    Date update

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND))
+    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND) + "" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY) + "." + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH) + "." + RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR))
 })
 input.onButtonPressed(Button.AB, function () {
     RTC_DS1307.DateTime(
     2022,
-    3,
+    11,
+    30,
+    11,
     22,
-    17,
-    41,
     0
     )
 })
