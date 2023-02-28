a7d5366f0dcf52ade95415896916a258718a7070
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2023-02-28 20:22:06 +0100

    Send time out on channel 255 every minute

--- main.ts
+++ main.ts
@@ -1,19 +1,39 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND) + "" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY) + "." + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH) + "." + RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR))
+    Pause = true
+    basic.clearScreen()
+    basic.pause(200)
+    SmallDigits.ShowNumber(RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR), LeadingZeroEnum.zz)
+    basic.pause(1000)
+    SmallDigits.ShowNumber(RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE), LeadingZeroEnum.zz)
+    basic.pause(1000)
+    Pause = false
 })
 input.onButtonPressed(Button.AB, function () {
     RTC_DS1307.DateTime(
-    2022,
-    11,
-    30,
-    11,
-    22,
+    2023,
+    2,
+    28,
+    18,
+    47,
     0
     )
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND))
+    RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + 1)
+    RTC_DS1307.setTime(RTC_DS1307.TimeType.SECOND, 0)
 })
+let Pause = false
+Pause = false
+led.setBrightness(10)
+radio.setGroup(255)
 basic.forever(function () {
-	
+    if (RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND) == 0) {
+        radio.sendValue("Hour", RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR))
+        radio.sendValue("Minute", RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
+        basic.pause(1000)
+    }
+    if (!(Pause)) {
+        SmallDigits.ShowNumber(RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND), LeadingZeroEnum.zz)
+    }
+    basic.pause(100)
 })
